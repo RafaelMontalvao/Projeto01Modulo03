625328741630f42287beef9e27d963e195f72053
Rafael Brito <rushbig@hotmail.com>
2023-05-24 19:50:30 -0300

    Adcionando endPoint de listar assentos

--- src/main/java/tech/devin/house/aviacaoapi/service/AssentoService.java
+++ src/main/java/tech/devin/house/aviacaoapi/service/AssentoService.java
@@ -1,4 +1,23 @@
 package tech.devin.house.aviacaoapi.service;
 
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class AssentoService {
+
+    List<String> assentos = new ArrayList<>();
+
+
+     public List<String> obterAssentos() {
+
+        for (int fileira = 1; fileira <= 9; fileira++) {
+            for (char poltrona = 'A'; poltrona <= 'F'; poltrona++) {
+                String assento = fileira + String.valueOf(poltrona);
+                assentos.add(assento);
+            }
+        }return assentos;
+}
 }
